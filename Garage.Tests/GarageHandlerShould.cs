c8deadfa992d7fee418295a92615871fe8aa3217
John Lundgren <john.paul.lundgren@gmail.com>
2018-12-27 15:22:02 +0100

    Implemented tests

--- Garage.Tests/GarageHandlerShould.cs
+++ Garage.Tests/GarageHandlerShould.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using Xunit;
+using Xunit.Abstractions;
 using Garage.Biz;
 using Garage.Biz.Vehicles;
+using Garage.Data;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Garage.Tests
 {
     public class GarageHandlerShould
     {
+
+        //For being able to create output in the tests.
+        private readonly ITestOutputHelper _output;
+
+        public GarageHandlerShould(ITestOutputHelper output)
+        {
+            _output = output;
+        }
 
         [Fact]
         public void AddVehicle_CapacityOne_ReturnTrue()
@@ -51,6 +63,58 @@ namespace Garage.Tests
 
             //Assert
             Assert.Same(expected, actual);
+        }
+
+        [Fact]
+        public void GetVehiclesAndGroupByType()
+        {
+            //Arrange
+            IVehiclesDAO dao = new VehiclesBinary(); 
+            GarageHandler<Vehicle> sut = new GarageHandler<Vehicle>(garageCapacity: 10);
+            ICollection<Vehicle> vehicles = dao.RetrieveAllVehicles();
+            AddVehiclesToGarage(vehicles, sut);
+
+            //Act
+            var result = sut.GetVehiclesGroupedByType();
+
+            foreach (var item in result)
+            {
+                _output.WriteLine($"Key: {item.Key.Name}, Count: {item.Count()}");
+            }
+
+            //TODO: Create som real assertion for the test.
+            //Assert
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void FindVehiclesByPropertyValues()
+        {
+            //Arrange
+            IVehiclesDAO dao = new VehiclesBinary();
+            GarageHandler<Vehicle> sut = new GarageHandler<Vehicle>(garageCapacity: 10);
+            ICollection<Vehicle> vehicles = dao.RetrieveAllVehicles();
+            AddVehiclesToGarage(vehicles, sut);
+
+            var props = new List<Tuple<string, string>>
+            {
+                new Tuple<string, string>("Color", "black"),
+                new Tuple<string, string>("WheelsNr", "4")
+            };
+
+            //Act
+            var result = sut.FindVehiclesByPropertyValues(props);
+
+            //Assert
+            Assert.True(true);
+        }
+
+        private void AddVehiclesToGarage(ICollection<Vehicle> vehicles, GarageHandler<Vehicle> handler)
+        {
+            foreach (var vehicle in vehicles)
+            {
+                handler.Add(vehicle);
+            }
         }
     }
 }
