2cd70733f75368a8ee2d942ac6fc2932ae61531c
John Lundgren <john.paul.lundgren@gmail.com>
2018-12-28 16:00:21 +0100

    Start of findvehicles implementation.

---
+++ Garage/FindVehiclesMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Garage.Cmd
+{
+    internal class FindVehiclesMenu
+    {
+        private static FindVehiclesMenu _instance;
+        private readonly string _removeVehicleMenuPath = Path.Combine(Environment.CurrentDirectory, "TextFiles", "remove_vehicles_menu.txt");
+        private readonly string _removeVehicleMenu;
+    }
+}
